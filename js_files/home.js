086d5edc2384312f2027c8d9eb48110d836a3ce7
solocyberengineer <solocyberengineer@gmail.com>
2023-10-11 06:56:34 +0200

    Navigation is now on all pages

---
+++ js_files/home.js
@@ -0,0 +1,35 @@
+/*
+"Hug me and I'll hug you back, I promise.",
+"A hug is always the right size.",
+"Where's my cuddle buddy when I need it?",
+"Hug the teddy and feel the coziness of heaven beside you!",
+"A fluffy friend that will always make you smile!",
+"A bear knows all your secrets - ands keeps them.",
+"Hug this one!",
+"A bedroom without a teddy is like a face without a smile.",
+"Try me!",
+"Teddy bears dont need hearts, they already stuffed with love.",
+"Pick me!",
+"People say nothing is impossible, but i do nothing everyday.",
+"You will feel comfort when you hug me".
+
+*/
+
+let comments = [
+	"Hug me and I'll hug you back, I promise.",
+	"A hug is always the right size.",
+	"Where's my cuddle buddy when I need it?",
+	"Hug the teddy and feel the coziness of heaven beside you!",
+	"A fluffy friend that will always make you smile!",
+	"A bear knows all your secrets - ands keeps them.",
+	"Hug this one!",
+	"A bedroom without a teddy is like a face without a smile.",
+	"Try me!",
+	"Teddy bears dont need hearts, they already stuffed with love.",
+	"Pick me!",
+	"People say nothing is impossible, but i do nothing everyday.",
+	"You will feel comfort when you hug me".
+]
+
+console.log(comments[0])
+
